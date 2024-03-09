cfd6dbc6d5bced86c6e0ffd1bd7d18de86d11d7b
diego.ferrari <diegoferrari02022022@gmail.com>
2024-03-09 07:32:28 -0300

    third commit

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -9,9 +9,25 @@ export class FormCalcComponent {
   num1: number = 0;
   num2: number = 0;
   sum : number = 0;
+  select = document.getElementById("opcoes") as HTMLSelectElement;
+
 
   onClicksum(){
     this.sum = this.num1 + this.num2;
   }
-
+  onCLicksubtraction(){
+    this.sum = this.num1 - this.num2;
+  }
+  onCLickmultiplication(){
+    this.sum = this.num1 * this.num2;
+  }
+  onClickdivision(){
+    this.sum = this.num1 / this.num2;
+  }
+  onClickpotentiation(){
+    this.sum = this.num1 ** this.num2;
+  }
+  onCLickrest(){
+    this.sum = this.num1 % this.num2;
+  }
 }
