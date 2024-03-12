7763309360e11a74d9f1a7a25d0203980bf54172
Diego <diegosferrari@outlook.com>
2024-03-11 21:09:24 -0300

    Função de botões adicionado

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -8,16 +8,63 @@ import { Component } from '@angular/core';
 export class FormCalcComponent {
   num1: number = 0;
   num2: number = 0;
-  sum : number = 0;
   selectedOption: string = '';
   result: number | null = null;
+  activeInput: string = '';
 
 
-  playSound() {
-    const audio = new Audio();
-    audio.src = '../assets/som.mp3';
-    audio.load();
-    audio.play();
+  addToNum(value: number) {
+    if (this.activeInput === 'num1') {
+      if (this.num1 === null) {
+        this.num1 = value;
+      } else {
+        this.num1 = parseInt(this.num1.toString() + value.toString());
+      }
+    } else if (this.activeInput === 'num2') {
+      if (this.num2 === null) {
+        this.num2 = value;
+      } else {
+        this.num2 = parseInt(this.num2.toString() + value.toString());
+      }
+    }
+  }
+
+  clearNums() {
+    if (this.activeInput === 'num1') {
+      this.num1 = 0;
+    } else if (this.activeInput === 'num2') {
+      this.num2 = 0;
+    }
+  }
+
+  handleNum1KeyDown(event: KeyboardEvent) {
+    const key = event.key;
+    if (!isNaN(Number(key))) {
+      if (this.num1 === null) {
+        this.num1 = Number(key);
+      } else {
+        this.num1 = parseInt(this.num1.toString() + key);
+      }
+    } else if (key === 'Backspace') {
+      this.num1 = Math.floor(this.num1 / 10);
+    }
+  }
+
+  handleNum2KeyDown(event: KeyboardEvent) {
+    const key = event.key;
+    if (!isNaN(Number(key))) {
+      if (this.num2 === null) {
+        this.num2 = Number(key);
+      } else {
+        this.num2 = parseInt(this.num2.toString() + key);
+      }
+    } else if (key === 'Backspace') {
+      this.num2 = Math.floor(this.num2 / 10);
+    }
+  }
+
+  handleInputFocus(fieldName: string) {
+    this.activeInput = fieldName;
   }
 
   calculate() {
